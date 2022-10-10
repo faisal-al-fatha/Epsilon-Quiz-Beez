9bd870de7f63c6ebbc32832c34feea17cc58558a
faisal-al-fatha <faysal313@gmail.com>
2022-10-11 04:46:43 +0600

    Home component added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -37,7 +37,7 @@ const Home = () => {
                     <div className="flex items-center">
                         <a
                             href="/"
-                            className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                            className="inline-flex items-center justify-center h-12 px-6 mr-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-purple-700 hover:bg-purple-900 focus:shadow-outline focus:outline-none"
                         >
                             Get started
                         </a>
--- src/Utilitis/routes.js
+++ src/Utilitis/routes.js
@@ -1,7 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Blogs from '../Components/Blogs';
 import ErrorPage from '../Components/ErrorPage';
 import Home from '../Components/Home';
 import Root from '../Components/Root';
+import Statistics from '../Components/Statistics';
+import Topics from '../Components/Topics';
 
 const router = createBrowserRouter([{
     path: '/',
@@ -15,6 +18,18 @@ const router = createBrowserRouter([{
         {
             path: '/home',
             element: <Home></Home>
+        },
+        {
+            path: '/topics',
+            element: <Topics></Topics>
+        },
+        {
+            path: '/statistics',
+            element: <Statistics></Statistics>
+        },
+        {
+            path: '/blogs',
+            element: <Blogs></Blogs>
         },
     ]
 }])
