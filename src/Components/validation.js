d190e4b8c60c3633c03f80f767111c972e4cede7
faisal-al-fatha <faysal313@gmail.com>
2022-10-12 20:46:02 +0600

    overall ok

--- src/Components/validation.js
+++ src/Components/validation.js
@@ -16,7 +16,7 @@ const Validation = ({ option, correctAnswer }) => {
         <div>
             <li>
                 <input type="radio" id={option} name="hosting" value={option} className="hidden peer" required="" />
-                <label onClick={(event) => validateAnswer(option)} for={option} className="inline-flex justify-between items-center p-5 h-24 w-full text-gray-900 bg-white rounded-lg border border-gray-700 cursor-pointer  peer-checked:bg-rose-500 peer-checked:border-rose-900 peer-checked:text-gray-900 hover:text-gray-600 hover:bg-gray-300 ">
+                <label onClick={(event) => validateAnswer(option)} for={option} className="inline-flex justify-between items-center p-5 h-24 w-full text-gray-900 bg-white rounded-lg border border-gray-700 cursor-pointer  peer-checked:bg-emerald-600 peer-checked:border-rose-900 peer-checked:text-black hover:text-gray-600 hover:bg-gray-300 ">
                     <div className="block">
                         <div className="w-full text-lg font-semibold">{option}</div>
                     </div>
